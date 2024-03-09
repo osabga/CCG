0f268fbcd8a1bdf63a757290aa5cf56de61369af
FRiJ0LITO <a00835376@tec.mx>
2024-03-08 18:53:29 -0600

    Integración de un modelo RAG con langchain y creación API con FastApi

--- chat-bot/main.py
+++ chat-bot/main.py
@@ -3,19 +3,20 @@ from fastapi import FastAPI
 from dotenv import dotenv_values
 from pymongo import MongoClient
 from routes import router as book_router
-from typing import Annotated
 
 config = dotenv_values(".env")
 
 
 @asynccontextmanager
 async def lifespan(FastAPI):
-    app.mongodb_client = MongoClient(config["ATLAS_URI"])
-    app.database = app.mongodb_client[config["DB_NAME"]]
-    print("Connected to the MongoDB database!")
-    yield
-    app.mongodb_client.close()
-    print("Close")
+    try:
+        app.mongodb_client = MongoClient(config["ATLAS_URI"])
+        app.database = app.mongodb_client[config["DB_NAME"]]
+        print("Connected to the MongoDB database!")
+        yield
+    finally:
+        app.mongodb_client.close()
+        print("Close")
 
 
 app = FastAPI(lifespan=lifespan)
@@ -26,9 +27,9 @@ async def root():
     return {"message": "Welcome"}
 
 
-app.include_router(book_router, tags=["books"], prefix="/book")
+app.include_router(book_router, tags=["pruebas"], prefix="/api/v1")
 
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run('main:app', port=8082)
+    uvicorn.run('main:app', port=8082, reload=True)
