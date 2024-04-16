d036090306d677658fb10ed0d458560004707020
FRiJ0LITO <a00835376@tec.mx>
2024-04-15 18:42:49 -0600

    Agregado soporte CORS para permitir solicitudes desde dominios cruzados

--- chat-bot/main.py
+++ chat-bot/main.py
@@ -1,8 +1,9 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import dotenv_values
 from pymongo import MongoClient
-from routes import router as book_router
+from routes import router as chat_router
 
 config = dotenv_values(".env")
 
@@ -21,15 +22,27 @@ async def lifespan(FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def root():
     return {"message": "Welcome"}
 
 
-app.include_router(book_router, tags=["pruebas"], prefix="/api/v1")
+app.include_router(chat_router, tags=["pruebas"], prefix="/api/v1")
 
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run('main:app', port=8082, reload=True)
+    uvicorn.run('main:app', host='0.0.0.0', port=8082, reload=True)
