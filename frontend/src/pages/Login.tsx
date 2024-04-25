e064a9385552e4dfdd8b03e16cb17a201508d7cb
Adrián Tadeo Barrera Almanza <tadeo34.barrera@gmail.com>
2024-04-24 18:17:48 -0600

    login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,8 +1,33 @@
 import Header from "../components/Header"
 import LoginImage from '../assets/Login_image.png'
 import { Link } from "react-router-dom"
+import axios from 'axios';
+import { useState } from "react";
 
+interface LoginData {
+  email: string;
+  password: string;
+}
 const Login = () => {
+  const [loginData, setLoginData] = useState<LoginData>({ email: '', password: '' });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setLoginData({ ...loginData, [name]: value });
+  };
+
+  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3000/api/login', loginData);
+      console.log(response.data);
+
+      // Aquí manejarías la respuesta de la API, como guardar el token, navegar a otra página, etc.
+    } catch (error) {
+      console.error('Error logging in:', error);
+    }
+  };
+
   return (
     <div className="bg-gradient-to-b ">
       <Header/>
@@ -18,11 +43,13 @@ const Login = () => {
           <p className="text-gray-400 mb-8">
             Login and access to answer your questions
           </p>
-          <form className="space-y-5">
+          <form onSubmit={handleLogin}  className="space-y-5">
             <div>
               <input
                 type="text"
                 placeholder="Email address"
+                onChange={handleChange}
+                  value={loginData.email}
                 className="w-full p-4 bg-gray-700 text-white rounded focus:ring-2 focus:ring-purple-600 focus:outline-none"
               />
             </div>
@@ -30,6 +57,8 @@ const Login = () => {
               <input
                 type="text"
                 placeholder="Password"
+                onChange={handleChange}
+                  value={loginData.password}
                 className="w-full p-4 bg-gray-700 text-white rounded focus:ring-2 focus:ring-purple-600 focus:outline-none"
               />
             </div>
@@ -53,7 +82,6 @@ const Login = () => {
           <div>
             <Link to='/Signup'>
               <button
-                type="submit"
                 className="mb-4 mt-4 w-full p-4 bg-gradient-to-r from-purple-600 to-blue-500 text-white rounded font-bold hover:opacity-90 transition-opacity duration-300"
               >
                 Create Account
