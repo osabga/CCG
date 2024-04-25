ea13a5e9a51f68f44b3e50e6d2700e3a099ce2b8
FRiJ0LITO <a00835376@tec.mx>
2024-04-24 18:55:29 -0600

    Question Model modified, answer optional and date attribute to find recent question

--- chat-bot/models.py
+++ chat-bot/models.py
@@ -2,6 +2,7 @@ import uuid
 from typing import Optional
 from pydantic import BaseModel, Field
 from typing import List
+from datetime import datetime
 
 
 class User(BaseModel):
@@ -29,14 +30,14 @@ class User(BaseModel):
 class Question(BaseModel):
     id: str = Field(default_factory=lambda: str(uuid.uuid4()), alias="_id")
     question: str = Field(...)
-    answer: str = Field(...)
+    answer: Optional[str] = Field("", description="Answer to the question")
     userId: str = Field(...)
+    date: datetime = Field(default_factory=datetime.now)
 
     class Config:
         json_schema_extra = {
             "example": {
                 "question": "What is NEORIS?",
-                "answer": "",
                 "userId": "82d00a97-d923-4c5a-bc8e-e1684eff66a9"
             }
         }
