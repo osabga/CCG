2b3bc984c4e8f78401d8868f191a72bbe25bd0a0
Adrián Tadeo Barrera Almanza <tadeo34.barrera@gmail.com>
2024-04-29 22:01:20 -0600

    29/04/2024

--- frontend/src/components/CardQuestion.tsx
+++ frontend/src/components/CardQuestion.tsx
@@ -1,3 +1,5 @@
+import pregunta from '../assets/pregunta.png'
+
 const CardQuestion = () => {
   const cardInfo = [
     {
@@ -24,11 +26,17 @@ const CardQuestion = () => {
 
   return (
     <div className="flex flex-wrap justify-center gap-8 p-4">  
-      {cardInfo.map((card) => (
-        <div className="bg-darkblue rounded-lg shadow-md p-6 max-w-sm w-full overflow-hidden" >  
+      {cardInfo.map((card, index) => (
+        <div key={index} className="bg-darkblue rounded-lg shadow-lg p-6 w-full max-w-4xl overflow-hidden flex items-center space-x-4 relative">
+        {/* Pseudo-elemento para el efecto superior */}
+        <div className="absolute top-0 left-0 w-[100%] h-[1.5px] bg-gradient-to-l from-darkblue via-blue-500 to-darkblue"></div>
+        
+        <img src={pregunta} alt="Question" className="h-6 w-6" />
+        <div className="pl-2">
           <div className="font-bold text-white mb-2">{card.question}</div>
           <div className="text-white">{card.answer}</div>
         </div>
+      </div>
       ))}
     </div>
   );
--- frontend/src/pages/FrequentlyQuestions.tsx
+++ frontend/src/pages/FrequentlyQuestions.tsx
@@ -5,7 +5,7 @@ function FrequentlyQuestions() {
   return (
     <div className="flex flex-col min-h-screen bg-gradient-to-d from-gray-100 to-gray-300">
       <Header />
-      <h1 className="hero-subtitle m-4">Frequently Asked Questions</h1>
+      <h1 className="hero-subtitle ml-20 mt-12 mb-12">Frequently Asked Questions</h1>
       <div className="flex-grow flex items-center justify-center">
         <CardQuestion />
       </div>
