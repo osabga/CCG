5282edacea7b1ccc87269a7248f8244208d3c4f3
FRiJ0LITO <a00835376@tec.mx>
2024-04-29 19:08:21 -0600

    Las cards se visualizan de mejor manera, bug solucionado

--- frontend/src/pages/FrequentlyQuestions.tsx
+++ frontend/src/pages/FrequentlyQuestions.tsx
@@ -1,26 +1,16 @@
-import React from 'react'
-import Header from '../components/Header'
-import CardQuestion from '../components/CardQuestion'
+import Header from '../components/Header';
+import CardQuestion from '../components/CardQuestion';
 
 function FrequentlyQuestions() {
   return (
-    <div className="bg-gradient-to-d ">
-    <Header/>
-
-      <section className="bg-yellow text-yellow flex justify-between items-center p-10 space-x-72 mb-[-10rem]">
-      <div className="space-y-4 w-[70vw] lg:mt-[-40rem] ">
-        <h1 className="hero-subtitle">Frequently Asked Questions</h1>
-
-        
+    <div className="flex flex-col min-h-screen bg-gradient-to-d from-gray-100 to-gray-300">
+      <Header />
+      <h1 className="hero-subtitle m-4">Frequently Asked Questions</h1>
+      <div className="flex-grow flex items-center justify-center">
+        <CardQuestion />
       </div>
-      <div className="relative flex items-center h-screen justify-center">
-        <CardQuestion/> 
-
-       
-      </div>
-    </section>
     </div>
-  )
+  );
 }
 
-export default FrequentlyQuestions
+export default FrequentlyQuestions;
