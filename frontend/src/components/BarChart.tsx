e765e34cec7278aa96f8949b6f94f8c61ffe19d1
FRiJ0LITO <a00835376@tec.mx>
2024-06-03 13:06:06 -0600

    Variable FC declarada pero no utilizada

--- frontend/src/components/BarChart.tsx
+++ frontend/src/components/BarChart.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import { useTranslation } from 'react-i18next';
