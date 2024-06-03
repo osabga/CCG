19829c959bce5a18729f30cd9613ac91d45699c5
FRiJ0LITO <a00835376@tec.mx>
2024-06-03 13:04:44 -0600

    Gráfica Dinámica funcionando

--- frontend/src/components/Chart.tsx
+++ frontend/src/components/Chart.tsx
@@ -8,6 +8,8 @@ import {
   Tooltip,
   Legend,
 } from 'chart.js';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Line } from 'react-chartjs-2';
 import { useTranslation } from 'react-i18next';
 
@@ -30,7 +32,7 @@ const options = {
       ticks: {
         autoSkip: true,
         maxRotation: 90,
-        minRotation: 45, // Ajusta según sea necesario para evitar superposiciones
+        minRotation: 45, 
       },
     },
     y: {
@@ -47,25 +49,42 @@ const options = {
   },
 };
 
+interface QuestionDay {
+  date: string;
+  count: number;
+}
+
 function Chart() {
   const { t } = useTranslation();
+  const [questionDay, setQuestionDay] = useState<QuestionDay[]>([]);
+  
+  const getToken = () => localStorage.getItem('token');
+  const headers = {
+    Authorization: `Bearer ${getToken()}`,
+    'Content-Type': 'application/json',
+  };
 
-  const labels = [
-    t('monday'),
-    t('tuesday'),
-    t('wednesday'),
-    t('thursday'),
-    t('friday'),
-    t('saturday'),
-    t('sunday'),
-  ];
+  useEffect(() => {
+    axios.get('https://neorisprueba.onrender.com/api/v1/questions/day', { headers })
+      .then(response => {
+        console.log('Received questions per day:', response.data);
+        setQuestionDay(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the questions per day!', error);
+      });
+  }, []);
+
+  // Transforma los datos para que sean compatibles con Chart.js
+  const labels = questionDay.map(item => item.date);
+  const dataCounts = questionDay.map(item => item.count);
 
   const data = {
     labels,
     datasets: [
       {
         label: t('questions_per_day'),
-        data: [255, 450, 290, 500, 260, 700, 400],
+        data: dataCounts,
         fill: false,
         borderColor: 'rgba(255, 255, 255, 1)', // Color del borde de la línea
         tension: 0.1, // Suavidad de la línea
